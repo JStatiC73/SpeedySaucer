cacd887e95ec8abd85aeec366970fa601574478a
Joshua Orellana <joshorellana73@gmail.com>
2025-01-05 23:21:57 -0600

    Se agregaron funciones a los scripts de player para movimiento e impulso

--- player.gd
+++ player.gd
@@ -3,9 +3,12 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#apply_impulse(Vector2(25, 25))
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _physics_process(delta):
+	apply_force(Vector2(25, 0))
