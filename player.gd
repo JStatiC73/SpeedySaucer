ac9c1458622d17a9b07f566d63f249df3eaf360e
StatiCPC <joshorellana73@gmail.com>
2025-01-07 01:37:05 -0600

    Se agrego la camara para seguir al jugador y los controles para detectar moviemientos del jugador

--- player.gd
+++ player.gd
@@ -1,14 +1,31 @@
 extends RigidBody2D
-
+var force = 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#apply_impulse(Vector2(25, 25))
-	pass
+	# apply_impulse(Vector2(25, 25))
+	# var force = 10
+	# para acceder a la variable force local = print(force)
+	# para acceder a la variable force global = print(self.force)
+	# print(force)
+	# print(self.force)
+	var result = add(5, 6)
+	print(result)
 
 
 func _process(delta):
 	pass
 	
 func _physics_process(delta):
-	apply_force(Vector2(25, 0))
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(force, 0))
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-force, 0))
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0, -force))
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0, force))
+
+func add(number1, number2):
+	var result = number1 + number2
+	return result;
